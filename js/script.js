0c7c98b9e0794af2e09fa4ef19a7275040d23f22
Abdur-RahmaanJ <arj.python@gmail.com>
2020-03-16 13:07:07 +0400

    v2

--- js/script.js
+++ js/script.js
@@ -1,15 +1,4 @@
 $(function() {
     console.log( "ready!" );
-    $(".close_but").click
-    (
-        function(){
-            $(this).parent().parent().remove();
-        }
-    );
-    $(".notice_close").click
-    (
-        function(){
-            $(this).parent().parent().remove();
-        }
-    );
+    
 });//END
