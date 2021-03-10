cd8d24c51163a4a59885ddcfbd9789eb27d947b9
Abdur-RahmaanJ <arj.python@gmail.com>
2021-03-10 22:46:49 +0400

    using py

---
+++ settings.py
@@ -0,0 +1,56 @@
+OUTPUT_FOLDER = 'docs/'
+
+
+testimonials = [
+    {
+        'name': 'Javed Mahamoodally',
+        'title': 'ServiceNow Developer | MRes Applied Mathematics',
+        'text': '''I have worked with Abdur-Rahmaan on different programming assignments in C++ and data science projects in Python.
+
+His exceptional understanding of coding paradigms, ability to grasp high-level material quickly and his outstanding communication skills make him a great lad to work with.
+
+I have benefited extensive from his deep understanding of pandas, NumPy and SciPy libraries of Python on my work in Artificial Neural Network.
+
+He would be a great fit any organisation or team'''
+    },
+    {
+        'name': 'Gico Carlo Evangelista',
+        'title': 'Software Engineering Intern at Outward, Inc.',
+        'text': '''I have worked with Abdur-Rahmaan on one of his Open Source Python projects on GitHub. He has helped mentored me in not only understanding the project thoroughly, but has also taught me the basics of contributing to an open source repository. He is very helpful when it comes to answering questions to both experienced developers and newcomers who are interested in learning about open source. Abdur-Rahmaan is a great mentor who I recommend working with.'''
+    },
+    {
+        'name': 'Nathan Heron - Morris',
+        'title': 'Junior Software Engineer at Dream Agility',
+        'text': '''Abdur-Rahmaan and his open source project has helped me not only with confidence in my coding skills but, also helped me think about the effects of explaining and documenting my contributions so others can understand what I have contributed. Abdur-Rahmaan has also been willing to answer any questions I may of had without judgement and provided me with some education in design principles.
+
+From the confidence Abdur-Rahmaan has helped me with. I have managed to land my first role as a software engineer with a great company that is solving problems with Artificial intelligence.
+
+So I would just like to say thank you.'''
+    },
+    {
+        'name': 'Angelo Giacco',
+        'title': 'Computer Science Student at Imperial College London',
+        'text': '''Abdur-Rahmaan is a great mentor. He is very patient when onboarding new members into open source projects and his communication skills are second to known. His vast experience is clear through the effective advice that he gives. He is very good at managing teams and I consider him an expert in all things Python. I hope to work more with him in the future! '''
+    },
+    {
+        'name': 'Hans Maulloo',
+        'title': 'Information Technology Trainee at SWAN Mauritius',
+        'text': '''I had the pleasure of working with Abdur-Rahmaan for the FlaskCon 2020. He was a great team player, well organized and very consistent when it came to managing the event. We had organising members from different corners of the world and Abdur made sure remote meetups were organised on a regular basis and would always follow the agenda. Even when facing technical difficulties, he was always ready to help out. I'd gladly work with Abdur on any related project :)'''
+    },
+    {
+        'name': 'Chukwudi Nwachukwu',
+        'title': 'Software Architect',
+        'text': '''Abdur-Rahmaan Janhangeer came off to me as someone who is well organised and gets job done. He does his best in all things and I adore him for that. I highly recommend him because I have worked together with him and his attention to detail was excellent. Well done Abdur-Rahmaan.'''
+    },
+    {
+        'name': 'Shamsuddin Rehmani',
+        'title': 'MS in Computer Science at University of Southern California',
+        'text': '''I worked with Abdur-Rahmaan's on one of his many open source software project, Shopyo. As this was my first time contributing to open source, I was not very confident in my skills. However, Abdur-Rahmaan was very patient and helped with the process of project setup and contribution. Looking back since my first open source contribution, I couldn't have asked for a better mentor than Abdur-Rahmaan. As I worked more with him, I learned that he has a vast knowledge not only of software engineering but also possess excellent communication and leadership skills. His attention to details, clear guidance, pursuit of best industry practices, and past experience and success in managing a team of engineers makes him a great leader
+and asset to any team.
+
+As if this was not enough, he also possess the gift of being artistic and creative, with the ability to make software not only functional but also marketable with design skills that he pursues as a hobby.
+
+His work in both professional and open source setting speaks itself but this recommendation is just a token of appreciation on behalf of countless engineers whom he has helped improve and break into the open source community. I wish him the best in his future endeavors and I am excited to see where his work takes him.'''
+    },
+]
+
